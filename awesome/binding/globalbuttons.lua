ffa605ab0ae12d1d5634e8371e4212bdb91d05d5
Epsi R. Nurwijayadi <epsi.nurwijayadi@gmail.com>
2016-07-14 15:38:50 +0700

    awesome: refactoring

--- awesome/binding/globalbuttons.lua
+++ awesome/binding/globalbuttons.lua
@@ -6,7 +6,7 @@ local _M = {}
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 function _M.get()
   local globalbuttons = awful.util.table.join(
-      awful.button({ }, 3, function () mymainmenu:toggle() end),
+      awful.button({ }, 3, function () RC.mainmenu:toggle() end),
       awful.button({ }, 4, awful.tag.viewnext),
       awful.button({ }, 5, awful.tag.viewprev)
   )
