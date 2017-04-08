1323c3725b8e91e2a5074481a10fffaaf355e7a9
Epsi R. Nurwijayadi <epsi.nurwijayadi@gmail.com>
2017-04-08 16:22:28 +0700

    standalone dzen2 bash

---
+++ standalone/dzen2/bash/example-04-main.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# remove all dzen2 instance
+pkill dzen2
+
+# include
+. ~/Documents/standalone/dzen2/bash/example-04-output.sh
+
+# dzen2
+
+xpos=0
+ypos=0
+width=640
+height=24
+fgcolor=$colBlack
+bgcolor=$colWhite
+font="-*-fixed-medium-*-*-*-12-*-*-*-*-*-*-*"
+
+parameters="  -x $xpos -y $ypos -w $width -h $height" 
+parameters+=" -fn $font"
+parameters+=" -ta c -bg $bgcolor -fg $fgcolor"
+parameters+=" -title-name dzentop"
+
+generated_output | dzen2 $parameters &
+
+sleep 1 && exec `(transset-df .8 -n dzentop >/dev/null 2>&1 &)` &
+
+
+
