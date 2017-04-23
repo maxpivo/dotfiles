b8c6109eb1766aaeee2ab3075a5bf01fb55ac8c8
Epsi R. Nurwijayadi <epsi.nurwijayadi@gmail.com>
2017-04-23 19:01:44 +0700

    standalone: debug

---
+++ standalone/lang/assets/debug.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+generated_output() {
+    conky -c ~/Documents/standalone/dzen2/debug/conky.lua
+}
+
+# ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----
+# execute
+
+clear
+generated_output
+
+
